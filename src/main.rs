7c924145292edd85da5b27374b16401759e0f7ec
Chris West (Faux) <git@goeswhere.com>
2022-03-24 19:45:41 +0000

    failing: well, it's not the async stuff

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,21 @@
-#[derive(Default)]
-struct Cli {}
-
-struct Tran<'a> {
-    client: &'a mut Cli,
+struct Holder<'a> {
+    parent: &'a mut (),
 }
 
-impl Cli {
-    async fn tran(&mut self) -> Tran<'_> {
-        Tran { client: self }
-    }
-}
-
-impl<'c> Tran<'c> {
-    async fn commit(self) {}
-}
-
-impl Drop for Tran<'_> {
+impl Drop for Holder<'_> {
     fn drop(&mut self) {}
 }
 
-async fn async_main() {
-    let mut client = Cli::default();
-    let mut tran = client.tran().await;
+fn main() {
+    let mut parent = ();
+    let mut tran = Holder {
+        parent: &mut parent,
+    };
 
     loop {
-        tran.commit().await;
-        tran = client.tran().await;
+        drop(tran);
+        tran = Holder {
+            parent: &mut parent,
+        };
     }
 }
-
-fn main() {}
